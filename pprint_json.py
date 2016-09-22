851c85f2c766eb8dd3596fdf07a20b1ca72ced1a
shabakka <nwolfegwow@gmail.com>
2016-09-22 23:52:52 +0300

    Update pprint_json.py
    зачем платить больше?

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
+import pprint
 
 
 def load_data(filepath):
+    with open(filepath, encoding='UTF-8') as file:
+        return json.load(file)
     pass
 
 
 def pretty_print_json(data):
+    lst = load_data(data)
+    return pprint.pprint(lst)
     pass
 
 
 if __name__ == '__main__':
+    path = 'D:/test/alco.json'
+    pretty_print_json(path)
     pass
